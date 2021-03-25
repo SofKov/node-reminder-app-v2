2b0c91b87cbdeee75a01484d37c207761ac4da78
Sofia K <sofiakovalevskaia@gmail.com>
2021-03-25 10:59:20 +0000

    updated app.js

--- src/app.js
+++ src/app.js
@@ -63,24 +63,3 @@ const app = async () => {
 
 
 main();
-
-
-
-
-
-
-
-// const command = process.argv[2];
-
-// if (command == "add"){
-//     console.log("Adding new note:");
-//     addNote(yargs.argv.note);
-// } else if (command == "remove"){
-//     console.log("Removing note:");
-//     removeNote(yargs.argv.note);
-// } else if (command == "list"){
-//     console.log("All notes are displayed below:");
-//     listNotes();
-// } else {
-//     console.log("Command not recognised");
-// };
